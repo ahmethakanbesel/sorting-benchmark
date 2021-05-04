e04f909b4d59d4c5acd160f8706627ef4b71d16e
Semir Tatlı <62341982+semirtatli@users.noreply.github.com>
2021-05-04 21:52:16 +0300

    CountingSort implemented

--- src/CountingSort.java
+++ src/CountingSort.java
@@ -5,13 +5,36 @@ public class CountingSort implements SortingAlgorithm {
     int arrayLength = 0;
 
     @Override
-    public int[] sort(int[] array) {
-        int[] count = new int[10000];
-        for (int i = arrayLength - 1; i > 0; i--) {
-            count[array[i]]++;
-            basicOperations++;
+    public int[] sort(int[] arr) {
+        int arrayLength = arr.length;
+        if (arrayLength == 0)
+            return arr;
+        /** find maximum and minimum values **/
+        int max = arr[0], min = arr[0];
+        for (int i = 1; i < arrayLength; i++) {
+            if (arr[i] > max)
+                max = arr[i];
+            if (arr[i] < min)
+                min = arr[i];
+            basicOperations += 2;
         }
-        return count;
+        int range = max - min + 1;
+
+        int[] count = new int[range];
+        /** initialize the occurrence of each element in the count array **/
+        for (int i = 0; i < arrayLength; i++)
+            count[arr[i] - min]++;
+        /** calculate sum of indexes **/
+        for (int i = 1; i < range; i++)
+            count[i] += count[i - 1];
+        /** modify original array according to the sum count **/
+        int j = 0;
+        for (int i = 0; i < range; i++)
+            while (j < count[i]) {
+                arr[j++] = i + min;
+                basicOperations++;
+            }
+        return arr;
     }
 
     @Override
